ef6bd320965e54ea2157cf000457e29f8a4fd685
JaiS739 <jksingh00739@gmail.com>
2023-03-16 19:44:39 +0530

    medals-widget

---
+++ src/medal_widget/config.cljs
@@ -0,0 +1,4 @@
+(ns medal-widget.config)
+
+(def debug?
+  ^boolean goog.DEBUG)
---
+++ src/medal_widget/core.cljs
@@ -0,0 +1,24 @@
+(ns medal-widget.core
+  (:require
+   [reagent.dom :as rdom]
+   [re-frame.core :as re-frame]
+   [medal-widget.events :as events]
+   [medal-widget.views :as views]
+   [medal-widget.config :as config]
+   ))
+
+
+(defn dev-setup []
+  (when config/debug?
+    (println "dev mode")))
+
+(defn ^:dev/after-load mount-root []
+  (re-frame/clear-subscription-cache!)
+  (let [root-el (.getElementById js/document "app")]
+    (rdom/unmount-component-at-node root-el)
+    (rdom/render [views/main-panel] root-el)))
+
+(defn init []
+  (re-frame/dispatch-sync [::events/initialize-db])
+  (dev-setup)
+  (mount-root))
